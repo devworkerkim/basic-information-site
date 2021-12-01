c1faeaa5be7a5facf2d01b8e831a95aa71e59f14
Kim Leung <71901468+devworkerkim@users.noreply.github.com>
2021-11-30 19:24:16 -0700

    Secure environmental variables

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config();
+
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
